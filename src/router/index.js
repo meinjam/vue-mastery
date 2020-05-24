aa1f910756f35a8270bcb3ec6cae37f4ec34716f
meinjam <injam.bd.jsr@gmail.com>
2020-05-24 12:04:04 +0600

    router, axios, slots and many more

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,34 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "EventList",
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/event/:id",
+    name: "EventShow",
+    component: EventShow,
+    props: true,
+  },
+  {
+    path: "/event/create",
+    name: "EventCreate",
+    component: EventCreate,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/EventList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" v-bind:event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((resp) => (this.events = resp.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style scoped></style>
